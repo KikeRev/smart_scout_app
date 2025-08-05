c4ccc832662b78f7063d4287a9fdb5ff194b81e2
KikeRev <enrique.revuelta@bain.com>
2025-08-05 14:11:24 +0200

    Add profile page, and collapsable menu to home page

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginView, LogoutView, SignUpView
+from .views import LoginView, LogoutView, SignUpView, ProfileView, ProfileUpdateView
 
 app_name = "users"                     
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path("login/",  LoginView.as_view(),  name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("signup/", SignUpView.as_view(), name="signup"),
+
+    path("profile/",        ProfileView.as_view(),        name="profile"),
+    path("profile/edit/",   ProfileUpdateView.as_view(),  name="profile_edit"),
 ]
